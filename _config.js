99b87155874233f3ea58f60096b9a074cb22b81c
0097eo <emmanuelokello294@gmail.com>
2024-11-18 19:27:25 +0300

    update config

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://okelo:kothbiro@cluster0.b3e32.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0',
-    development: 'mongodb+srv://okelo:kothbiro@cluster0.b3e32.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Cluster0',
-    test: 'mongodb+srv://okelo:kothbiro@cluster0.b3e32.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Cluster0',
+    production: 'mongodb+srv://okelo:kothbiro@cluster0.b3e32.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://okelo:kothbiro@cluster0.b3e32.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://okelo:kothbiro@cluster0.b3e32.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
